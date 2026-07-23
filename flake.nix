9d16e586a63aebb313562c1b0b609741d615f6b1
João Paulo Seregatte Costa <seregatte@gmail.com>
2026-07-23 09:47:49 -0300

    chore: bump version to 0.0.12

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
   outputs = { self, nixpkgs }:
     let
-      version = "0.0.11";
+      version = "0.0.12";
 
       # Platform-specific SHA-256 hashes (updated by release workflow)
       platformHashes = {
